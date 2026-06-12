ef4fffdf670784aa81fc029432cb1faba2ae6de5
1ming-ir <yiming3415@gmail.com>
2026-06-12 13:42:56 +0800

    Update CorsConfig.java

--- src/main/java/com/campus/health/config/CorsConfig.java
+++ src/main/java/com/campus/health/config/CorsConfig.java
@@ -1,4 +1,4 @@
-﻿package com.campus.health.config;
+package com.campus.health.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
